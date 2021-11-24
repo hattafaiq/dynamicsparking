48b669d8a94e9c21abe1fde1444fa4d8df2ed8f9
hattafaiq <hattafaiq@gmail.com>
2021-11-24 22:10:58 +0700

    coba metode lain

---
+++ elemen.py
@@ -0,0 +1,11 @@
+deret = [6, 7, 8 , 9, 100, 10, 11, 11, 11]
+jumlah=int(input("datamasuk= "))
+isi=[]
+for i in range(1,jumlah):
+    isi.append(i)
+    print(isi)
+#deret2 = [1, 2, 3, 4, 5]
+#deret.extend(deret2)
+#deret.sort()
+
+#print(deret)
