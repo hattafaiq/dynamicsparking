0906856596f76a8e194a54b27cb88d3b48bef2da
hattafaiq <hattafaiq@gmail.com>
2021-11-24 16:28:35 +0700

    mencoba

---
+++ dynamicparcking.py
@@ -0,0 +1,13 @@
+#def inputmobil(inputan):
+
+def main():
+    while True:
+        inputan=""
+        lahan=
+        slot=range(200):
+        jumlah=slot+inputan
+        print(slot)
+        inputan=input("masukan jumlah mobil")
+    
+if __name__ == '__main__':
+    main()
